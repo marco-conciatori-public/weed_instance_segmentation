4a5854a425c655a8193e3345b9ada57b9ecb2f9a
Marco <irotaicnoc@gmail.com>
2026-02-04 21:41:36 +0100

    updated paths

--- SorghumWeedDataset_Segmentation/Mask2Former_inference.py
+++ SorghumWeedDataset_Segmentation/Mask2Former_inference.py
@@ -15,7 +15,8 @@ def load_model():
     # 'facebook/mask2former-swin-large-coco-instance' is the high-accuracy
     # checkpoint fine-tuned for instance segmentation.
     # model_id_or_path = 'facebook/mask2former-swin-large-coco-instance'
-    model_id_or_path = 'models/mask2former_finetuned/final_model'
+    model_id_or_path = 'models/mask2former_finetuned/2026-02-04_20-56-07/best_model'
+    # model_id_or_path = 'models/mask2former_finetuned/2026-02-04_20-56-07/final_model'
     print(f'Loading {model_id_or_path}...')
 
     processor = AutoImageProcessor.from_pretrained(model_id_or_path, use_fast=False)
