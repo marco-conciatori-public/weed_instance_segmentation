6a2e5f05c37be519d90f759daf2172d2bcd92706
Marco <irotaicnoc@gmail.com>
2026-02-11 21:34:27 +0100

    refactoring

--- config.py
+++ config.py
@@ -1,15 +1,20 @@
-# Global Training Configuration
+# Global Configuration
 
+# Training
 MODEL_CHECKPOINT = 'facebook/mask2former-swin-large-coco-instance'
 BATCH_SIZE = 2
 LEARNING_RATE = 5e-5
 EPOCHS = 100
 GRADIENT_ACCUMULATION = 2
+
+# Data
 MAX_INPUT_DIM = 1024
 MAX_IMAGES = None  # Set to None for full dataset, or an integer for debugging
-OUTPUT_DIR = 'C:/Users/Marco/GIT/weed_instance_segmentation/output/'
-MODELS_OUTPUT_DIR = OUTPUT_DIR + 'models/'
 DATASET_LIST = [
     'sorghum_weed',
     'pheno_bench',
 ]
+
+# Output Directories
+OUTPUT_DIR = 'C:/Users/Marco/GIT/weed_instance_segmentation/output/'
+MODELS_OUTPUT_DIR = OUTPUT_DIR + 'models/'
