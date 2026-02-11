c50d58a138b0426247243d6cdcb58afc71d5a80b
Marco <irotaicnoc@gmail.com>
2026-02-12 00:43:30 +0100

    changed parameters

--- config.py
+++ config.py
@@ -11,9 +11,9 @@ GRADIENT_ACCUMULATION = 2
 MAX_INPUT_DIM = 1024
 MAX_IMAGES = None  # Set to None for full dataset, or an integer for debugging
 DATASET_LIST = [
-    'sorghum_weed',
+    # 'sorghum_weed',
     'pheno_bench',
-    'crop_weed',
+    # 'crop_weed',
 ]
 FORCE_PREPROCESSING = True
 
