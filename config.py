b5efb3e0dabcaa15e067f5a063ba2aa98fddd798
Marco <irotaicnoc@gmail.com>
2026-02-11 18:11:17 +0100

    updated config

--- config.py
+++ config.py
@@ -9,4 +9,7 @@ MAX_INPUT_DIM = 1024
 MAX_IMAGES = None  # Set to None for full dataset, or an integer for debugging
 OUTPUT_DIR = 'C:/Users/Marco/GIT/weed_instance_segmentation/output/'
 MODELS_OUTPUT_DIR = OUTPUT_DIR + 'models/'
-DATASET_LIST = ['sorghum_weed']
+DATASET_LIST = [
+    'sorghum_weed',
+    'pheno_bench',
+]
